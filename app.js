f6fc7ffb858852fe5231861cf63ecb3afa6257f0
Julia Malinina <malinina.artwork@gmail.com>
2023-02-26 17:22:16 +0100

    set first request header

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 function requestListener(request, response) {
   console.log(request.url, request.method, request.headers);
+  response.setHeader('Content-Type', 'text/html');
+  response.write('<html>');
+  response.write('<head><title>My first page</title></head>');
+  response.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  response.write('</html');
+
+  response.end();
 }
 
 const server = http.createServer(requestListener);
